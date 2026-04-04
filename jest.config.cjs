243bd77bcce95566a268a1672c4781257002d325
rb3z3rra <sbezerra.rodrigo@gmail.com>
2026-04-04 18:03:32 -0400

    test: CRUD de equipamentos

---
+++ jest.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import("jest").Config} **/
+module.exports = {
+  preset: "ts-jest/presets/default-esm",
+  testEnvironment: "node",
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
+};
