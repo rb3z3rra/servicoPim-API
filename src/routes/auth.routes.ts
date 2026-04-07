33122a9e6066226a2e3cab16a5c393f303ec5dd4
patrese <patrese.ebs@gmail.com>
2026-04-06 21:55:57 -0400

    feat: implement refresh token route with validation and integration tests

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -2,13 +2,17 @@ import { Router } from "express";
 import { AuthController } from "../controllers/AuthController.js";
 import { asyncHandler } from "../middleware/asyncHandler.js";
 import { validarBody } from "../middleware/validarBody.js";
-import { loginSchemaDTO } from "../dtos/AuthDTO.js";
+import { loginSchemaDTO, refreshTokenSchemaDTO } from "../dtos/AuthDTO.js";
 
 const authRoutes = Router();
 const authController = new AuthController();
 
 authRoutes.post("/login",  validarBody(loginSchemaDTO),
   asyncHandler(authController.login.bind(authController))
+);
+
+authRoutes.post("/refresh", validarBody(refreshTokenSchemaDTO),
+  asyncHandler(authController.refresh.bind(authController))
 );
 
 export { authRoutes };
